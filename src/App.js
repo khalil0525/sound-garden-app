be29524b7a38ea75ade3b40ca880ae86f8974a9d
khalil0525 <collinskhalil@hotmail.com>
2022-04-07 20:09:12 -0400

    Added skeleton for app, grid and some styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import styles from "./App.module.css";
+import { BrowserRouter, Routes, Route, NavLink, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <div className={styles.sidebar}>
+        <div className={styles.navigation}>
+          <BrowserRouter>
+            <div className={styles["navigation-main"]}>
+              <h2>Menu</h2>
+              <nav className={styles.navbar}>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/genres">Genres</NavLink>
+                <NavLink to="/artists">Artists</NavLink>
+              </nav>
+            </div>
+            <div className={styles["navigation-library"]}>
+              <h2>Library</h2>
+              <nav className={styles.navbar}>
+                <NavLink to="/liked">Liked</NavLink>
+                <NavLink to="/uploaded">Uploaded</NavLink>
+              </nav>
+            </div>
+            <Routes>
+              <Route path="/"></Route>
+              <Route path="/genres"></Route>
+              <Route path="/artists"></Route>
+              <Route path="/liked"></Route>
+              <Route path="/uploaded"></Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </div>
+      <div className={styles.page}>right</div>
     </div>
   );
 }
