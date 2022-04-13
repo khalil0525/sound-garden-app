516874aab86a5b9643c2c743654df7f6cc716790
khalil0525 <collinskhalil@hotmail.com>
2022-04-13 07:04:35 -0400

    Updated some text in Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae qui
         deserunt expedita quod recusandae porro a quaerat impedit, unde
         doloremque aut culpa praesentium eum suscipit itaque earum rerum nulla?
-        Corrupti.
+        Corrupti.ddd
       </p>
       <ActionBar></ActionBar>
     </div>
