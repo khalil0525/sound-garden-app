1ce87abbb5bf86a7b961c8828b849ef853e4ed69
khalil0525 <collinskhalil@hotmail.com>
2022-04-15 19:33:41 -0400

    Changed some text in Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae qui
         deserunt expedita quod recusandae porro a quaerat impedit, unde
         doloremque aut culpa praesentium eum suscipit itaque earum rerum nulla?
-        Corrupti.ddd
+        Corrupti.ddddddd
       </p>
       <ActionBar></ActionBar>
     </div>
