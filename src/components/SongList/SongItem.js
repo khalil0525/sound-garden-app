039970ae43e88fb42e965dad36f10bdf08d34366
khalil0525 <collinskhalil@hotmail.com>
2022-04-24 19:30:48 -0400

    Added 2 new component files, SongList and SongItem along with their modules

---
+++ src/components/SongList/SongItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./SongItem.module.css";
+const SongItem = () => {
+  return (
+    <>
+      <li></li>
+    </>
+  );
+};
+
+export default SongItem;
---
+++ src/components/SongList/SongList.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./SongList.module.css";
+const SongList = ({ type }) => {
+  return <ul></ul>;
+};
+
+export default SongList;
