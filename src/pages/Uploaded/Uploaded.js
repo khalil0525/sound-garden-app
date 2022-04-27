d513adbdd737de8972a50d68d151b61094ff54b3
khalil0525 <collinskhalil@hotmail.com>
2022-04-26 23:24:47 -0400

    Added ActionBar to uploaded page

--- src/pages/Uploaded/Uploaded.js
+++ src/pages/Uploaded/Uploaded.js
@@ -3,15 +3,15 @@ import SongList from "../../components/SongList/SongList";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useCollection } from "../../hooks/useCollection";
 import styles from "./Uploaded.module.css";
-
+import ActionBar from "../../components/ActionBar/ActionBar";
 export default function Uploaded() {
   const { user } = useAuthContext();
   const { documents, error } = useCollection("music", ["uid", "==", user.uid]);
 
   return (
     <div className={styles.uploaded}>
-      <h1>Uploaded Tracks</h1>
-
+      <ActionBar />
+      <h1 className={styles["header_text"]}>Uploaded Tracks</h1>
       <SongList songs={documents} />
     </div>
   );
