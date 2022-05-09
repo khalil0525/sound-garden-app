5d4baedb750cb89add74e76f9f09ee738e47fb41
khalil0525 <collinskhalil@hotmail.com>
2022-05-08 23:56:40 -0400

    Implemented 2 way binding in actionsearchbar

--- src/components/ActionBar/ActionSearchBar.js
+++ src/components/ActionBar/ActionSearchBar.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import searchIcon from "../../images/Search.svg";
 import styles from "./ActionSearchBar.module.css";
+
 const ActionSearchBar = () => {
+  const [searchText, setSearchText] = useState("");
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+  };
+  useEffect(() => {
+    console.log(searchText);
+  }, [searchText]);
   return (
     <div className={styles.actionsearchbar}>
       <div className={styles["actionsearchbar__contents"]}>
         <img src={searchIcon} alt="Search button icon" />
-        <input type="text" placeholder="Type here to search" />
+        <input
+          type="text"
+          placeholder="Type here to search"
+          value={searchText}
+          onChange={handleSearchTextChange}
+        />
       </div>
     </div>
   );
