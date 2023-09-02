461be44b56e6c03d96b3bb0a3de49201426984d7
Khalil Collins <Collinskhalil@hotmail.com>
2023-09-02 16:40:59 -0400

    Updated card definition, removed firebase.json from .gitignore

--- src/components/CardList/Card.js
+++ src/components/CardList/Card.js
@@ -1,12 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types"; // Import PropTypes for prop type validation
 import styles from "./Card.module.css";
 
 const Card = (props) => {
+	const { background, title, content } = props;
+
 	return (
-		<li className={styles.card}>
-			<div className={styles["card__title"]}>
-				<p>{props.title && props.title.toUpperCase()}</p>
-			</div>
+		<li className={`${styles.card} ${background ? styles[background] : ""}`}>
+			{title && (
+				<div className={styles["card__title"]}>
+					<p>{title.toUpperCase()}</p>
+				</div>
+			)}
+			{content && <div className={styles["card__content"]}>{content}</div>}
 		</li>
 	);
 };
+
+// Define propTypes for prop type validation
+Card.propTypes = {
+	background: PropTypes.string, // Background CSS class name
+	title: PropTypes.string, // Title text
+	content: PropTypes.node, // Content (can be JSX)
+};
+
 export default Card;
