84dc87c288a476c4f7c49a3ae46c52a644777255
Khalil Collins <Collinskhalil@hotmail.com>
2023-11-05 03:48:21 -0500

    prettier

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import { AuthContextProvider } from "./context/AuthContext";
-import { AudioPlayerContextProvider } from "./context/AudioPlayerContext";
-import "./fonts/Inter/Inter-Medium.ttf";
-import "./fonts/Inter/Inter-Regular.ttf";
-import "./fonts/Inter/Inter-Bold.ttf";
-import "./fonts/Inter/Inter-SemiBold.ttf";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './styles/index.css';
+import App from './App';
+import { AuthContextProvider } from './context/AuthContext';
+import { AudioPlayerContextProvider } from './context/AudioPlayerContext';
+import './fonts/Inter/Inter-Medium.ttf';
+import './fonts/Inter/Inter-Regular.ttf';
+import './fonts/Inter/Inter-Bold.ttf';
+import './fonts/Inter/Inter-SemiBold.ttf';
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
-	<React.StrictMode>
-		{/* Custom context provider component */}
-		<AudioPlayerContextProvider>
-			<AuthContextProvider>
-				<App />
-			</AuthContextProvider>
-		</AudioPlayerContextProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    {/* Custom context provider component */}
+    <AudioPlayerContextProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </AudioPlayerContextProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
