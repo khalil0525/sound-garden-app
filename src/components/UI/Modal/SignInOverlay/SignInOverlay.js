3b2dfd7f67689c4945c04d4a01948585bc089b98
Khalil Collins <Collinskhalil@hotmail.com>
2023-11-07 02:42:52 -0500

    styled sign in overlay and added validation

--- src/components/UI/Modal/SignInOverlay/SignInOverlay.js
+++ src/components/UI/Modal/SignInOverlay/SignInOverlay.js
@@ -1,32 +1,66 @@
-import styles from "./SignInOverlay.module.css";
-import { useState } from "react";
-import { useLogin } from "../../../../hooks/useLogin";
+import styles from './SignInOverlay.module.css';
+import { useState } from 'react';
+import { useLogin } from '../../../../hooks/useLogin';
 
 const SignInOverlay = (props) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
   const { login, error, isPending } = useLogin();
+  console.log(isPending);
+  const validateEmail = (value) => {
+    // Basic email validation using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!value.match(emailRegex)) {
+      setEmailError('Invalid email address');
+      return false;
+    }
+    setEmailError('');
+    return true;
+  };
+
+  const validatePassword = (value) => {
+    if (value.length < 6) {
+      setPasswordError('Password must be at least 6 characters');
+      return false;
+    }
+    setPasswordError('');
+    return true;
+  };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    // try to log user in
-    login(email, password);
+
+    // Reset any previous validation errors
+    setEmailError('');
+    setPasswordError('');
+
+    // Validate email and password
+    const isEmailValid = validateEmail(email);
+    const isPasswordValid = validatePassword(password);
+
+    // If both email and password are valid, attempt to log in
+    if (isEmailValid && isPasswordValid) {
+      login(email, password);
+    }
   };
 
   return (
     <div className={styles.modal}>
       <form
-        className={styles["signInForm"]}
+        className={styles['signInForm']}
         onSubmit={handleFormSubmit}>
         <h2>Sign in</h2>
 
-        <div className={styles["signInForm_controls"]}>
+        <div className={styles['signInForm_controls']}>
           <input
             type="email"
             value={email}
             placeholder="Your Email Address"
             onChange={(e) => setEmail(e.target.value)}
           />
+          {emailError && <p className={styles.error}>{emailError}</p>}
 
           <input
             type="password"
@@ -34,9 +68,10 @@ const SignInOverlay = (props) => {
             placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
           />
+          {passwordError && <p className={styles.error}>{passwordError}</p>}
         </div>
 
-        <div className={styles["signInForm_actions"]}>
+        <div className={styles['signInForm_actions']}>
           {!isPending && <button type="submit">Login</button>}
           {isPending && (
             <button
@@ -45,11 +80,11 @@ const SignInOverlay = (props) => {
               Loading
             </button>
           )}
-          {/* <button>Register</button> */}
           {error && <p>{error}</p>}
         </div>
       </form>
     </div>
   );
 };
+
 export default SignInOverlay;
