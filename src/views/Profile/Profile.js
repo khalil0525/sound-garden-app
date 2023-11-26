ba9f8e687a9d3b17d6382c885114ba3ae2ead0b7
Khalil Collins <Collinskhalil@hotmail.com>
2023-11-26 10:05:34 -0500

    Created backup of audio player

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,70 +1,145 @@
-import styles from './Profile.module.css';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Skeleton from '@mui/material/Skeleton';
+import Modal from '../../components/UI/Modal/Modal';
 import { useLogout } from '../../hooks/useLogout';
 import { useAuthContext } from '../../hooks/useAuthContext';
-import editIcon from '../../images/pencil_solid.svg';
-import { useEffect, useState } from 'react';
-import Modal from '../../components/UI/Modal/Modal';
-// import { useCollection } from '../../hooks/useCollection';
-import Skeleton from '@mui/material/Skeleton';
-import Button from '../../components/UI/Button/Button';
-import { useParams } from 'react-router-dom';
-import placeholderImage from '../../images/profile_placeholder.svg';
-import CollectionResults from '../../components/CollectionResults/CollectionResults';
-
 import { followUser, getUserProfile, unfollowUser } from '../../api/functions';
 import Layout from '../../components/Layout/Layout';
+import placeholderImage from '../../images/profile_placeholder.svg';
+import CollectionResults from '../../components/CollectionResults/CollectionResults';
+import { makeStyles } from '@mui/styles';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { Box } from '@mui/material';
+
+const tabs = ['All', 'Tracks', 'Playlists', 'Liked', 'Reposted'];
+
+const useStyles = makeStyles((theme) => ({
+  profile__content: {
+    padding: theme.spacing(2),
+  },
+  profile__headerContainer: {
+    maxWidth: '124rem',
+    margin: '0 auto',
+  },
+  profile__header: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: '1.6rem',
+    padding: '3rem',
+    background: '#ccc',
+    borderRadius: '1.6rem !important',
+  },
+  profileHeaderImageContainer: {
+    width: '20rem',
+    height: '20rem',
+    marginRight: '3rem',
+    backgroundColor: '#fff',
+    borderRadius: '50%',
+    overflow: 'hidden',
+  },
+  profileHeaderImageContainer_img: {
+    width: '100%',
+    height: '100%',
+    borderRadius: '50%',
+  },
+  profileHeaderContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: '1rem',
+  },
+  profileHeaderTextContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    gap: '0.4rem',
+  },
+  profileHeaderDisplayName: {
+    color: '#fff',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    display: 'inline-block',
+    fontSize: '2rem',
+    lineHeight: '3.7rem',
+    padding: '0.4rem 0.7rem',
+  },
+  profileHeaderName: {
+    color: '#fff',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    display: 'inline-block',
+    fontSize: '1.5rem',
+    lineHeight: '1.2',
+    padding: '0.4rem 0.7rem',
+  },
+  profileHeaderEditContainer: {
+    maxWidth: '12rem',
+    width: '12rem',
+    height: '150px',
+    position: 'relative',
+    '&:hover $editContainer-updateBtn': {
+      display: 'inline-block',
+      opacity: 0.8,
+    },
+  },
+  editContainer_updateBtn: {
+    display: 'none',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '1rem',
+    padding: '0.2rem 0.6rem',
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.text.primary,
+    maxWidth: '12rem',
+  },
+  editContainer_updateBtn_focused: {
+    display: 'inline-block',
+  },
+  editContainer_updateBtn_hover: {
+    opacity: 1,
+  },
+  editContainer_menu: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+  },
+  profile__profileActions: {
+    display: 'flex',
+
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0.4rem 0',
+    padding: '0.4rem 2rem 0.8rem',
+    borderBottom: '1px solid #ccc',
+    width: '100%',
+  },
+}));
 
 export default function Profile({ scrollRef }) {
+  const classes = useStyles();
+  const { logout, error, isPending } = useLogout();
+  const { user } = useAuthContext();
   const [profile, setProfile] = useState(null);
-  // const [profileSongs, setProfileSongs] = useState(null);
   const [isEditingProfile, setIsEditingProfile] = useState(false);
-  // const [isEditingPlaylist, setIsEditingPlaylist] = useState(false);
-  const [isEditingHeader, setIsEditingHeader] = useState(false);
   const [updateButtonToggled, setUpdateButtonToggled] = useState(false);
   const [isProcessingFollow, setIsProcessingFollow] = useState(false);
   const [isFollowing, setIsFollowing] = useState(null);
-  // const [isFetchedSongs, setIsFetchedSongs] = useState(false);
-  const { logout, error, isPending } = useLogout();
-  const { user } = useAuthContext();
-
-  // const location = useLocation();
+  const [currentTab, setCurrentTab] = useState(0);
   const params = useParams();
   const URL = params.profileURL;
-  console.log(URL);
-  // const { documents: profileDocuments, error: perror } = useCollection('usrs', [
-  //   'profileURL',
-  //   '==',
-  //   URL,
-  // ]);
-  // // console.log(profileDocuments && profileDocuments.length);
   const query =
     profile && profile.userID
       ? ['music', ['userID', '==', profile.userID]]
       : null;
-
   const handleEditProfile = () => {
     setIsEditingProfile(false);
   };
-  // const handleEditPlaylist = () => {
-  //   setIsEditingPlaylist(false);
-  // };
-  // const handleEditHeader = () => {
-  // 	setIsEditingHeader(false);
-  // };
 
-  // const getProfileSongs = useCallback(async () => {
-  //   try {
-  //     const { data } = await getSongs({
-  //       where: { field: 'userID', operator: '==', value: profile.userID },
-  //     });
-
-  //     console.log(data);
-  //     setProfileSongs({ ...data });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, [profile]);
-  async function handleFollowClick() {
+  const handleFollowClick = async () => {
     setIsProcessingFollow(true);
     try {
       const { data } = profile.followers.includes(user.id)
@@ -79,16 +154,14 @@ export default function Profile({ scrollRef }) {
       console.error('Error:', error);
       setIsProcessingFollow(false);
     }
-  }
-
+  };
+  const handleTabChange = (event, newValue) => {
+    setCurrentTab(newValue);
+  };
   useEffect(() => {
     const getProfile = async () => {
       try {
         const { data } = await getUserProfile({ profileURL: URL });
-        // if (user) {
-        //   setIsFollowing(data.followers.includes(user.id));
-        // }
-        console.log(data);
         setProfile({ ...data });
       } catch (error) {
         console.log(error);
@@ -96,143 +169,190 @@ export default function Profile({ scrollRef }) {
     };
     getProfile();
   }, [URL, user]);
-
-  // useEffect(() => {
-  //   if (profile && profile?.userID && !isFetchedSongs) {
-  //     getProfileSongs();
-  //     setIsFetchedSongs(true);
-  //   }
-  // }, [profile, getProfileSongs, isFetchedSongs]);
-
-  return (
-    <Layout user={user}>
-      <div className={styles['profile__content']}>
-        <div className={styles['profile__headerContainer']}>
-          <div className={styles['profile__header']}>
-            <div className={styles['profile__header-imgContainer']}>
-              {profile && profile.profilePhotoURL ? (
-                <img
-                  src={
-                    profile.profilePhotoURL
-                      ? profile.profilePhotoURL
-                      : placeholderImage
-                  }
-                  alt="User profile"
-                />
-              ) : (
-                <>
-                  <Skeleton
-                    variant="circular"
-                    width={40}
-                    height={40}
-                  />
-                </>
-              )}
-            </div>
-            <div className={styles['profile__header-content']}>
-              <div className={styles['profile__header-textContainer']}>
-                <h2 className={styles['profile__header-displayName']}>
-                  {profile?.displayName || (
-                    <Skeleton
-                      width={60}
-                      height={20}
-                    />
-                  )}
-                </h2>
-                {profile?.firstName || profile?.lastName ? (
-                  <h3 className={styles['profile__header-firstLastName']}>
-                    {profile?.firstName + ' ' + profile?.lastName}
-                  </h3>
-                ) : (
-                  <Skeleton
-                    variant="rounded"
-                    width={60}
-                    height={20}
-                  />
-                )}
-              </div>
-              <div className={styles['profile__header-editContainer']}>
-                <Button
-                  onFocus={() => setUpdateButtonToggled(true)}
-                  onBlur={() => setUpdateButtonToggled(false)}
-                  // disabled={isEditingHeader}
-                  buttonSize="large"
-                  // iconImage={editIcon}
-
-                  altText="Profile edit Icon"
-                  className={`${styles['editContainer-updateBtn']} ${
-                    updateButtonToggled &&
-                    styles['editContainer-updateBtn--focused']
-                  }`}>
-                  Update image
-                </Button>
-                {/* { ( */}
-                {updateButtonToggled && (
-                  <ul className={styles['editContainer-menu']}>
-                    <Button buttonSize="large">Replace image</Button>
-                    <Button buttonSize="large">Delete image</Button>
-                  </ul>
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles['profile__profileActions']}>
-          {/* Logout button temp */}
-          {/* Logout button should only show when the :ProfileURL belongs to */}
-          {profile && user && user.id !== profile.userID && (
-            <Button
-              className={styles['profile__header_button_follow']}
-              onClick={handleFollowClick}
-              disabled={isProcessingFollow}
-              buttonSize="large">
-              {isFollowing ? 'Following' : 'Follow'}
-            </Button>
-          )}
-          {profile && user && user.uid === profile.userID ? (
-            <Button
-              className={styles['profile__header_button']}
-              onClick={() => setIsEditingProfile(true)}
-              disabled={isEditingProfile}
-              buttonSize="large"
-              iconImage={editIcon}
-              altText="Profile edit Icon">
-              Edit Profile
-            </Button>
-          ) : (
-            <Skeleton
-              variant="rounded"
-              width={210}
-              height={60}
-            />
-          )}
-          {profile &&
-            user &&
-            user.uid === profile.userID &&
-            (!isPending ? (
-              <Button
-                className={styles['profile__header_button']}
-                onClick={logout}
-                buttonSize="large">
-                Logout
-              </Button>
-            ) : (
-              <Button
-                className={styles['profile__header_button']}
-                disabled
-                buttonSize="large">
-                Loading..
-              </Button>
-            ))}
-          {error && <p>{error}</p>}
-        </div>
-        {profile && query && (
+  const renderTabContent = () => {
+    switch (currentTab) {
+      case 0:
+        return (
           <CollectionResults
             scrollRef={scrollRef}
             query={query}
           />
+        );
+      case 1:
+        return (
+          <CollectionResults
+            scrollRef={scrollRef}
+            query={query}
+          />
+        );
+      // Add cases for other tabs
+      default:
+        return null;
+    }
+  };
+  return (
+    <Layout user={user}>
+      <Grid
+        container
+        className={classes.profile__content}>
+        <Grid
+          item
+          xs={12}>
+          <Grid
+            container
+            className={classes.profile__headerContainer}>
+            <Grid
+              item
+              xs={12}
+              md={12}>
+              <Grid
+                container
+                className={classes.profile__header}>
+                <Grid
+                  item
+                  xs={12}
+                  md={3}>
+                  <div className={classes.profileHeaderImageContainer}>
+                    {profile && profile.profilePhotoURL ? (
+                      <img
+                        src={profile.profilePhotoURL}
+                        alt="User profile"
+                        className={classes.profileHeaderImageContainer_img}
+                      />
+                    ) : (
+                      <Skeleton
+                        variant="circular"
+                        width={40}
+                        height={40}
+                      />
+                    )}
+                  </div>
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  md={8}>
+                  <div className={classes.profileHeaderContent}>
+                    <div className={classes.profileHeaderTextContainer}>
+                      <h2 className={classes.profileHeaderDisplayName}>
+                        {profile?.displayName || (
+                          <Skeleton
+                            width={60}
+                            height={20}
+                          />
+                        )}
+                      </h2>
+                      {profile?.firstName || profile?.lastName ? (
+                        <h3 className={classes.profileHeaderName}>
+                          {profile?.firstName + ' ' + profile?.lastName}
+                        </h3>
+                      ) : (
+                        <Skeleton
+                          variant="rounded"
+                          width={60}
+                          height={20}
+                        />
+                      )}
+                    </div>
+                    <div className={classes.profileHeaderEditContainer}>
+                      <Button
+                        onFocus={() => setUpdateButtonToggled(true)}
+                        onBlur={() => setUpdateButtonToggled(false)}
+                        className={`${classes.editContainer_updateBtn} ${
+                          updateButtonToggled &&
+                          classes.editContainer_updateBtn_focused
+                        }`}>
+                        Update image
+                      </Button>
+                      {updateButtonToggled && (
+                        <ul className={classes.editContainer_menu}>
+                          <Button buttonSize="large">Replace image</Button>
+                          <Button buttonSize="large">Delete image</Button>
+                        </ul>
+                      )}
+                    </div>
+                  </div>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              xs={12}
+              width="100%">
+              <Box className={classes.profile__profileActions}>
+                <Tabs
+                  value={currentTab}
+                  onChange={handleTabChange}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  centered>
+                  {tabs.map((tab, index) => (
+                    <Tab
+                      key={index}
+                      label={tab}
+                    />
+                  ))}
+                </Tabs>
+                <Grid>
+                  {profile && user && user.id !== profile.userID && (
+                    <Button
+                      className={classes.profile__header_button_follow}
+                      onClick={handleFollowClick}
+                      disabled={isProcessingFollow}
+                      buttonSize="large">
+                      {isFollowing ? 'Following' : 'Follow'}
+                    </Button>
+                  )}
+                  {profile && user && user.uid === profile.userID ? (
+                    <Button
+                      className={classes.profile__header_button}
+                      onClick={() => setIsEditingProfile(true)}
+                      disabled={isEditingProfile}
+                      buttonSize="large">
+                      Edit Profile
+                    </Button>
+                  ) : (
+                    <Skeleton
+                      variant="rounded"
+                      width={210}
+                      height={60}
+                    />
+                  )}
+                  {profile &&
+                    user &&
+                    user.uid === profile.userID &&
+                    (!isPending ? (
+                      <Button
+                        className={classes.profile__header_button}
+                        onClick={logout}
+                        buttonSize="large">
+                        Logout
+                      </Button>
+                    ) : (
+                      <Button
+                        className={classes.profile__header_button}
+                        disabled
+                        buttonSize="large">
+                        Loading..
+                      </Button>
+                    ))}
+                  {error && <p>{error}</p>}
+                </Grid>
+              </Box>
+            </Grid>
+          </Grid>
+        </Grid>
+        {profile && (
+          <Grid
+            item
+            xs={12}>
+            <CollectionResults
+              scrollRef={scrollRef}
+              query={query}
+            />
+          </Grid>
         )}
-      </div>
+      </Grid>
 
       {isEditingProfile && (
         <Modal
@@ -242,20 +362,6 @@ export default function Profile({ scrollRef }) {
           onCancel={() => setIsEditingProfile(false)}
         />
       )}
-      {/* {isEditingPlaylist && (
-        <Modal
-          action="editPlaylistInformation"
-          userInformation={profile}
-          onConfirm={handleEditProfile}
-          onCancel={() => setIsEditingPlaylist(false)}
-        />
-      )} */}
     </Layout>
   );
-}
-{
-  /* <div className={styles['profile__notFound']}>
-<h1>We can’t find that user.</h1>
-<p>Please check the extension and try again!</p>
-</div> */
 }
