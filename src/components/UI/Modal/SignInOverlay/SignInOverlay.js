6e1ba5eb2adc0c7232b6c27995a18e626f454ef7
Khalil Collins <Collinskhalil@hotmail.com>
2023-12-12 10:05:18 -0500

    implemented forgot password

--- src/components/UI/Modal/SignInOverlay/SignInOverlay.js
+++ src/components/UI/Modal/SignInOverlay/SignInOverlay.js
@@ -1,4 +1,3 @@
-import styles from './SignInOverlay.module.css';
 import { useState } from 'react';
 import { useLogin } from '../../../../hooks/useLogin';
 import LockIcon from '@mui/icons-material/Lock';
@@ -12,16 +11,79 @@ import {
   Typography,
   Divider,
   InputAdornment,
-  Link,
 } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 import { ReactComponent as LogoIcon } from '../../../../images/logo.svg';
+import { makeStyles } from '@mui/styles';
+
 import theme from '../../../../theme.js';
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    position: 'absolute',
+    top: '10vh',
+    left: 0,
+    right: 0,
+    zIndex: 2000,
+    margin: '0 auto',
+    backgroundColor: 'transparent',
+    maxWidth: '40rem',
+    borderRadius: '18px',
+    padding: '2rem',
+    textAlign: 'center',
+  },
+  signInForm: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '1.5rem',
+    maxWidth: '100%',
+  },
+  signInFormHeaderText: {
+    margin: '0.8rem 0 !important',
+    fontSize: theme.typography.h2.fontSize,
+  },
+  signInFormControls: {
+    width: '100%',
+    backgroundColor: '#fff',
+  },
+  signInFormControlsInput: {
+    width: '100%',
+    fontSize: theme.typography.body1.fontSize,
+  },
+  signInFormActions: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+  signInFormActionsButton: {
+    display: 'block',
+    width: '100%',
+    backgroundColor: '#2196f3',
+    color: '#fff !important',
+    border: 'none',
+    padding: '14px 20px',
+
+    cursor: 'pointer',
+    fontSize: theme.typography.body1.fontSize,
+    borderRadius: '4px',
+  },
+  signInFormActionsButtonDisabled: {
+    backgroundColor: '#ccc',
+    cursor: 'not-allowed',
+  },
+}));
 const SignInOverlay = (props) => {
+  const classes = useStyles(theme);
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [emailError, setEmailError] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const { login, error, isPending, signInWithGoogle } = useLogin();
+  const [showPasswordRecovery, setShowPasswordRecovery] = useState(false); // New state for toggling password recovery
 
   const validateEmail = (value) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -55,9 +117,12 @@ const SignInOverlay = (props) => {
       login(email, password);
     }
   };
-
+  const handlePasswordRecoverySubmit = (event) => {
+    event.preventDefault();
+    // Implement password recovery logic here
+  };
   return (
-    <div className={styles.modal}>
+    <div className={classes.modal}>
       <Paper
         elevation={3}
         sx={{
@@ -65,103 +130,198 @@ const SignInOverlay = (props) => {
           maxWidth: 500,
           backgroundColor: theme.palette.secondary.main,
         }}>
-        <form
-          className={styles['signInForm']}
-          onSubmit={handleFormSubmit}>
-          <Grid
-            container
-            gap="1.6rem"
-            direction="column"
-            justifyContent="center"
-            alignItems="center">
+        {showPasswordRecovery ? (
+          <form
+            className={classes.signInForm}
+            onSubmit={handlePasswordRecoverySubmit}>
+            {/* Password Recovery Form */}
+
             <Grid
-              item
-              xs={12}
-              sx={{
-                display: 'flex',
-                width: '60%',
-                justifyContent: 'center',
-                flexDirection: 'column',
-                alignItems: 'center',
-                paddingTop: '20px',
-              }}>
-              <LogoIcon />
-              <Typography
-                variant="h1"
-                sx={{ color: '#000' }}>
-                SoundGarden
-              </Typography>
+              container
+              gap="1.6rem"
+              direction="column"
+              justifyContent="center"
+              alignItems="center">
+              {' '}
+              <Grid
+                item
+                xs={12}
+                sx={{
+                  display: 'flex',
+                  width: '60%',
+                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  paddingTop: '20px',
+                }}>
+                <LogoIcon />
+                <Typography
+                  variant="h1"
+                  className={classes.signInFormHeaderText}
+                  sx={{ color: '#fff' }}>
+                  SoundGarden
+                </Typography>
+              </Grid>
+              <Typography variant="body1">Password Recovery</Typography>{' '}
+              <Grid
+                item
+                xs={12}
+                className={classes.signInFormControls}>
+                <TextField
+                  variant="filled"
+                  fullWidth
+                  label="Email"
+                  value={email}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <EmailIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Grid>
             </Grid>
-            <Grid
-              item
-              xs={6}
-              className={styles['signInForm_controls']}>
-              <TextField
-                variant="filled"
-                fullWidth
-                label="Email"
-                value={email}
-                size="small"
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <EmailIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                sx={{ height: '40px', marginBottom: '1rem' }}
-              />
-            </Grid>
-            <Grid
-              item
-              xs={6}
-              className={styles['signInForm_controls']}>
-              {emailError && <p className={styles.error}>{emailError}</p>}
-              <TextField
-                variant="filled"
-                type="password"
-                value={password}
-                label="Password"
-                onChange={(e) => setPassword(e.target.value)}
-                sx={{ height: '40px', marginBottom: '1rem' }}
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <LockIcon />
-                    </InputAdornment>
-                  ),
-                }}
-              />
-              {passwordError && <p className={styles.error}>{passwordError}</p>}
-            </Grid>
-            <div className={styles['signInForm_actions']}>
-              {!isPending && <Button type="submit">Login</Button>}
-              {isPending && (
-                <Button
-                  className="btn"
-                  disabled>
-                  Loading
-                </Button>
-              )}
-              {error && <p>{error}</p>}
-            </div>
-            <Divider />
             <Button
-              variant="filled"
-              sx={{ backgroundColor: '#fff !important', color: 'black' }}
-              startIcon={<GoogleIcon />}
-              onClick={signInWithGoogle}>
-              Sign In with Google
+              className={classes.signInFormActionsButton}
+              variant="outlined"
+              fullWidth
+              type="submit">
+              Recover Password
             </Button>
-            <Link
-              href="/password-recovery"
-              color="inherit">
-              Forgot Password?
-            </Link>
-          </Grid>
-        </form>
+            <Button
+              onClick={() => setShowPasswordRecovery(false)}
+              variant="text"
+              sx={{ cursor: 'pointer', marginTop: '1rem', color: '#fff' }}
+              startIcon={<ArrowBackIcon />}>
+              Back to Login
+            </Button>
+          </form>
+        ) : (
+          <form
+            className={classes.signInForm}
+            onSubmit={handleFormSubmit}>
+            <Grid
+              container
+              gap="1.6rem"
+              direction="column"
+              justifyContent="center"
+              alignItems="center">
+              <Grid
+                item
+                xs={12}
+                sx={{
+                  display: 'flex',
+                  width: '60%',
+                  justifyContent: 'center',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  paddingTop: '20px',
+                }}>
+                <LogoIcon />
+                <Typography
+                  variant="h1"
+                  className={classes.signInFormHeaderText}
+                  sx={{ color: '#fff' }}>
+                  SoundGarden
+                </Typography>
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                className={classes.signInFormControls}>
+                <TextField
+                  variant="filled"
+                  fullWidth
+                  label="Email"
+                  value={email}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <EmailIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                  sx={{
+                    height: '40px',
+                    marginBottom: '1.6rem',
+                    padding: '0.4rem',
+                  }}
+                />{' '}
+                {emailError && <p className={classes.error}>{emailError}</p>}
+              </Grid>
+              <Grid
+                item
+                xs={12}
+                className={classes.signInFormControls}>
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="password"
+                  value={password}
+                  label="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  sx={{
+                    height: '40px',
+                    marginBottom: '1.6rem',
+                    padding: '0.4rem',
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockIcon />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+                {passwordError && (
+                  <p className={classes.error}>{passwordError}</p>
+                )}
+              </Grid>
+              <div className={classes.signInFormActions}>
+                {!isPending && (
+                  <Button
+                    className={classes.signInFormActionsButton}
+                    variant="outlined"
+                    fullWidth
+                    type="submit">
+                    Login
+                  </Button>
+                )}
+                {isPending && (
+                  <Button
+                    fullWidth
+                    disabled>
+                    Loading
+                  </Button>
+                )}
+                {error && <p>{error}</p>}
+              </div>
+              <Divider />
+              <div className={classes.signInFormActions}>
+                <Button
+                  variant="filled"
+                  fullWidth
+                  size="large"
+                  sx={{ backgroundColor: '#fff !important', color: 'black' }}
+                  startIcon={<GoogleIcon />}
+                  onClick={signInWithGoogle}>
+                  Sign In with Google
+                </Button>
+              </div>
+              <Button
+                variant="body3"
+                onClick={() => setShowPasswordRecovery(true)}
+                sx={{ cursor: 'pointer', marginTop: '1rem', color: '#fff' }}>
+                Forgot Password?
+              </Button>
+            </Grid>
+          </form>
+        )}
       </Paper>
     </div>
   );
